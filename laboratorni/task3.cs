3da6d92495bc14f8486a6bcd9877bc2bf9574d6a
olfnkk <olenafediv0@gmail.com>
2025-11-25 00:21:10 +0200

    2

---
+++ laboratorni/task3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorni
+{
+    internal class task3
+    {
+        public static void ThirdTask()
+        {
+            Console.Write("Введіть свій річний дохід (грн): ");
+            double income = double.Parse(Console.ReadLine());
+
+            double taxRate;
+
+            if (income <= 100000)
+            {
+                taxRate = 0.05;
+            }
+            else if (income <= 300000)
+            {
+                taxRate = 0.10;
+            }
+            else if (income <= 1000000)
+            {
+                taxRate = 0.20;
+            }
+            else
+            {
+                taxRate = 0.30;
+            }
+
+            double tax = income * taxRate;
+
+            Console.WriteLine($"\nСтавка податку: {taxRate * 100}%");
+            Console.WriteLine($"Сума податку: {tax:F2} грн");
+            Console.WriteLine($"До сплати залишається: {income - tax:F2} грн");
+        }
+    }
+}
