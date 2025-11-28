2a7bc27c0a0faf2ec8efdd0c64d0fdbcd9c431a6
olfnkk <olenafediv0@gmail.com>
2025-11-28 06:05:06 +0200

    eighth

--- laboratorni/Program.cs
+++ laboratorni/Program.cs
@@ -4,28 +4,28 @@ namespace laboratorni
 {
     internal class Figure
     {
-        public virtual double CalculateArea() => 0;
-        public virtual double CalculatePerimeter() => 0;
+        public virtual double CalculateArea() =>0;
+        public virtual double CalculatePerimeter() =>0;
     }
 
-    internal class Circle : Figure
+    internal class Circle:Figure
     {
-        public double Radius { get; set; }
+        public double Radius {get;set;}
 
         public Circle(double radius)
         {
             Radius = radius;
         }
 
-        public override double CalculateArea() => Math.PI * Radius * Radius;
+        public override double CalculateArea() => Math.PI*Radius*Radius;
 
-        public override double CalculatePerimeter() => 2 * Math.PI * Radius;
+        public override double CalculatePerimeter() => 2 * Math.PI*Radius;
     }
 
-    internal class Rectangle : Figure
+    internal class Rectangle:Figure
     {
-        public double Width { get; set; }
-        public double Height { get; set; }
+        public double Width {get; set;}
+        public double Height {get; set;}
 
         public Rectangle(double width, double height)
         {
@@ -33,14 +33,14 @@ namespace laboratorni
             Height = height;
         }
 
-        public override double CalculateArea() => Width * Height;
+        public override double CalculateArea() => Width*Height;
 
-        public override double CalculatePerimeter() => 2 * (Width + Height);
+        public override double CalculatePerimeter() => 2 * (Width+Height);
     }
 
     internal class Fruit
     {
-        public string Name { get; set; }
+        public string Name {get; set;}
 
         public Fruit(string name)
         {
@@ -50,10 +50,10 @@ namespace laboratorni
         public override string ToString() => $"Фрукт: {Name}";
     }
 
-    internal class Apple : Fruit
+    internal class Apple:Fruit
     {
-        public string Variety { get; set; }
-        public string Color { get; set; }
+        public string Variety {get; set;}
+        public string Color {get; set;}
 
         public Apple(string name, string variety, string color) : base(name)
         {
@@ -65,10 +65,10 @@ namespace laboratorni
             $"Яблуко: {Name}, Сорт: {Variety}, Колір: {Color}";
     }
 
-    internal class Pear : Fruit
+    internal class Pear:Fruit
     {
-        public string Variety { get; set; }
-        public string Color { get; set; }
+        public string Variety {get; set;}
+        public string Color {get; set;}
 
         public Pear(string name, string variety, string color) : base(name)
         {
@@ -83,8 +83,8 @@ namespace laboratorni
 
     public abstract class Animal
     {
-        public float Weight { get; set; }
-        public string Color { get; set; }
+        public float Weight {get; set;}
+        public string Color {get; set;}
 
         public Animal(string color, float weight)
         {
@@ -95,9 +95,9 @@ namespace laboratorni
         public abstract string MakeSound();
     }
 
-    public abstract class AnimalWithTail : Animal
+    public abstract class AnimalWithTail:Animal
     {
-        public float TailLength { get; set; }
+        public float TailLength {get; set;}
 
         public AnimalWithTail(string color, float weight, float tailLength)
             : base(color, weight)
@@ -106,7 +106,7 @@ namespace laboratorni
         }
     }
 
-    public class Cat : AnimalWithTail
+    public class Cat:AnimalWithTail
     {
         public Cat(string color, float weight, float tailLength)
             : base(color, weight, tailLength)
@@ -116,13 +116,13 @@ namespace laboratorni
         private string Purr() => "purrrrrrrr";
         private string Meow() => "Meow";
 
-        public override string MakeSound() => Purr() + Meow();
+        public override string MakeSound() => Purr()+Meow();
 
         public override string ToString() =>
-            $"This is a Cat, Color = {Color}, Weight = {Weight}, TailLength = {TailLength}";
+            $"This is a Cat, Color={Color}, Weight ={Weight}, TailLength={TailLength}";
     }
 
-    public class Dog : AnimalWithTail
+    public class Dog:AnimalWithTail
     {
         public Dog(string color, float weight, float tailLength)
             : base(color, weight, tailLength)
@@ -132,7 +132,7 @@ namespace laboratorni
         public override string MakeSound() => "Woof";
 
         public override string ToString() =>
-            $"This is a Dog, Color = {Color}, Weight = {Weight}, TailLength = {TailLength}";
+            $"This is a Dog, Color={Color}, Weight ={Weight}, TailLength={TailLength}";
     }
 
     internal class Program
@@ -143,8 +143,8 @@ namespace laboratorni
             Circle c = new Circle(5);
             Rectangle r = new Rectangle(4, 7);
 
-            Console.WriteLine($"Коло: площа = {c.CalculateArea():F2}, периметр = {c.CalculatePerimeter():F2}");
-            Console.WriteLine($"Прямокутник: площа = {r.CalculateArea():F2}, периметр = {r.CalculatePerimeter():F2}");
+            Console.WriteLine($"Коло: площа ={c.CalculateArea():F2}, периметр ={c.CalculatePerimeter():F2}");
+            Console.WriteLine($"Прямокутник: площа = {r.CalculateArea():F2}, периметр ={r.CalculatePerimeter():F2}");
 
             Console.WriteLine("\n 2.ФРУКТИ");
             Fruit f = new Fruit("Фрукт");
